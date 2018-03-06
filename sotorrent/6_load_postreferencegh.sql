67d4c3e989e4e67bbd0042d5160810b5cff9fa04
Sebastian Baltes <research@sbaltes.com>
2018-03-06 15:44:51 +0100

    Revert change of character set
    * Reason: ERROR 1300 (HY000): Invalid utf8 character string: ''

--- sotorrent/6_load_postreferencegh.sql
+++ sotorrent/6_load_postreferencegh.sql
@@ -3,7 +3,7 @@ USE `sotorrent17_12`;
 SET foreign_key_checks = 0;
 # load file exported from BigQuery (see also https://cloud.google.com/sql/docs/mysql/import-export/)
 LOAD DATA LOCAL INFILE 'PostReferenceGH.csv' INTO TABLE `PostReferenceGH`
-CHARACTER SET utf8
+CHARACTER SET utf8mb4
 FIELDS TERMINATED BY ','
 OPTIONALLY ENCLOSED BY '\"'
 ESCAPED BY '\"'
