1cf3ea57b8ae1dae1e76cd478d583dcfb95db438
Sebastian Baltes <research@sbaltes.com>
2018-04-10 16:52:15 +0930

    Add query to retrieve comments about Google

--- analysis/links/links_bq.sql
+++ analysis/links/links_bq.sql
@@ -29,3 +29,41 @@ GROUP BY Domain
 ORDER BY PostCount DESC;
 
 => JavaLinks.csv
+
+
+# retrieve post blocks of most recent version
+#standardsql
+SELECT *
+FROM (
+	SELECT *
+	FROM `sotorrent-org.2018_03_28`.`PostBlockVersion`
+	WHERE PostHistoryId IN (
+		SELECT MAX(PostHistoryId)
+		FROM `sotorrent-org.2018_03_28`.`PostVersion`
+		GROUP BY PostId
+	)
+);
+
+=> analysis_2018_03_28.PostBlockVersionRecent
+
+# lines with comments about Google
+#standardsql
+SELECT
+  Id,
+  PostId,
+  REGEXP_EXTRACT(LOWER(line), r'google(?:d){0,1}\s+(?:it|that)\s+') as Match,
+  Line
+FROM (
+  SELECT Id, PostId, Line
+  FROM (
+    SELECT Id, PostId, SPLIT(Content, "&#xD;&#xA;") as lines
+    FROM `sotorrent-org.analysis_2018_03_28`.`PostBlockVersionRecent`
+    WHERE PostBlockTypeId = 1  # only consider text blocks
+  )
+  CROSS JOIN UNNEST(lines) as Line
+)
+WHERE
+  REGEXP_CONTAINS(LOWER(Line), r'google(?:d){0,1}\s+(?:it|that)\s+');
+
+=> analysis_2018_03_28.GoogleLines
+
