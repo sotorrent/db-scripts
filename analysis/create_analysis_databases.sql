93d9f9933522fdda9be657e0a4e51446a5b5e22f
Sebastian Baltes <research@sbaltes.com>
2018-04-30 14:53:33 +0930

    Add first draft of script to draw sample for qualitative analysis

---
+++ analysis/create_analysis_databases.sql
@@ -0,0 +1,7 @@
+# create database for temporary tables
+SET NAMES utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE DATABASE `analysis` DEFAULT CHARACTER SET utf8mb4 DEFAULT COLLATE utf8mb4_unicode_ci;
+
+# create database for temporary tables
+SET NAMES utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE DATABASE `analysis_18_03` DEFAULT CHARACTER SET utf8mb4 DEFAULT COLLATE utf8mb4_unicode_ci;
--- analysis/create_database.sql
+++
@@ -1,3 +0,0 @@
-# create database for temporary tables
-SET NAMES utf8mb4 COLLATE utf8mb4_unicode_ci;
-CREATE DATABASE `analysis` DEFAULT CHARACTER SET utf8mb4 DEFAULT COLLATE utf8mb4_unicode_ci;
