bafc5243fbf8a2a42ebe9fed4261014a7044d9a2
Sebastian Baltes <research@sbaltes.com>
2019-04-04 13:37:40 +0200

    Update script to 7z

---
+++ sotorrent/export/calculate_md5.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+md5sum so-dump/Badges.xml.7z
+md5sum so-dump/Comments.xml.7z
+md5sum so-dump/PostHistory.xml.7z
+md5sum so-dump/PostLinks.xml.7z
+md5sum so-dump/Posts.xml.7z
+md5sum so-dump/Tags.xml.7z
+md5sum so-dump/Users.xml.7z
+md5sum so-dump/Votes.xml.7z
+
+md5sum gh-references/GHMatches.csv.7z
+md5sum gh-references/PostReferenceGH.csv.7z
+
+md5sum sotorrent/CommentUrl.csv.7z
+md5sum sotorrent/PostBlockDiff.csv.7z
+md5sum sotorrent/PostBlockVersion.csv.7z
+md5sum sotorrent/PostVersion.csv.7z
+md5sum sotorrent/PostVersionUrl.csv.7z
+md5sum sotorrent/TitleVersion.csv.7z
