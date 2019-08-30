8edd07bbb63b122c7141b054a1dac155b6d1715a
Sebastian Baltes <research@sbaltes.com>
2019-08-30 14:29:30 +0930

    Update script to release 2019-6-21

--- analysis/extended-version/mutual-references.sql
+++ analysis/extended-version/mutual-references.sql
@@ -1,43 +1,49 @@
-# retrieve posts and comments referencing GitHub repos
+--- retrieve posts and comments referencing GitHub repos
+
+#standardsql
 SELECT
   Id,
   PostId,
   PostHistoryId, 
   REGEXP_EXTRACT(Path, r'^([A-Za-z0-9_.-]+/[A-Za-z0-9_.-]+)') AS Repo,
   REGEXP_EXTRACT(Path, r'^[A-Za-z0-9_.-]+/[A-Za-z0-9_.-]+(.*)') AS Path
-FROM `sotorrent-org.2019_03_17.PostVersionUrl`
+FROM `sotorrent-org.2019_06_21.PostVersionUrl`
 WHERE RootDomain = "github.com"
   AND REGEXP_CONTAINS(Path, r'^[A-Za-z0-9_.-]+/[A-Za-z0-9_.-]+'); 
   
-=> sotorrent-extension.2019_03_17.PostVersionUrlGH
+=> so-tests.2019_06_21.PostVersionUrlGH
 
+#standardsql
 SELECT
   Id,
   PostId,
   CommentId, 
   REGEXP_EXTRACT(Path, r'^([A-Za-z0-9_.-]+/[A-Za-z0-9_.-]+)') AS Repo,
   REGEXP_EXTRACT(Path, r'^[A-Za-z0-9_.-]+/[A-Za-z0-9_.-]+(.*)') AS Path
-FROM `sotorrent-org.2019_03_17.CommentUrl`
+FROM `sotorrent-org.2019_06_21.CommentUrl`
 WHERE RootDomain = "github.com"
   AND REGEXP_CONTAINS(Path, r'^[A-Za-z0-9_.-]+/[A-Za-z0-9_.-]+'); 
   
-=> sotorrent-extension.2019_03_17.CommentUrlGH
+=> so-tests.2019_06_21.CommentUrlGH
 
+#standardsql
 SELECT
   PostId,
   Repo
-FROM `sotorrent-extension.mutual_references.PostVersionUrlGH`
+FROM `so-tests.2019_06_21.PostVersionUrlGH`
 GROUP BY PostId, Repo
 UNION DISTINCT
 SELECT
   PostId,
   Repo
-FROM `sotorrent-extension.mutual_references.CommentUrlGH`;
+FROM `so-tests.2019_06_21.CommentUrlGH`;
 
-=> sotorrent-extension.2019_03_17.SOLinksGH
+=> so-tests.2019_06_21.LinksFromSOToGH
 
 
-# retrieve posts referencing GitHub repos, which are themselves referenced from the repo
+--- retrieve posts referencing GitHub repos, which are themselves referenced from the repo
+
+#standardsql
 SELECT
   gh.Repo AS Repo,
   FileId,
@@ -45,10 +51,48 @@ SELECT
   gh.PostId AS PostId,
   gh.SOUrl AS SOUrl,
   gh.GHUrl AS GHUrl
-FROM `sotorrent-org.2019_03_17.PostReferenceGH` gh
-JOIN `sotorrent-extension.mutual_references.SOLinksGH` so
+FROM `sotorrent-org.2019_06_21.PostReferenceGH` gh
+JOIN `so-tests.2019_06_21.LinksFromSOToGH` so
 ON gh.Repo = so.Repo
 	AND gh.PostId = so.PostId
 GROUP BY Repo, FileId, FileExt, PostId, SOUrl, GHUrl;
 
-=> sotorrent-extension.2019_03_17.MutualReferences
+=> so-tests.2019_06_21.MutualReferences
+
+
+#standardsql
+SELECT
+  gh_threads.Repo AS Repo,
+  FileId,
+  FileExt,
+  gh_threads.ParentId AS ParentId,
+  gh_threads.PostId AS PostId,
+  SOUrl,
+  GHUrl
+FROM (
+  SELECT
+    gh.Repo AS Repo,
+    FileId,
+    FileExt,
+    threads1.ParentId,
+    gh.PostId AS PostId,
+    gh.SOUrl AS SOUrl,
+    gh.GHUrl AS GHUrl
+  FROM `sotorrent-org.2019_06_21.PostReferenceGH` gh
+  JOIN `sotorrent-org.2019_06_21.Threads` threads1
+  ON gh.PostId = threads1.PostId
+) AS gh_threads
+JOIN (
+  SELECT 
+    so.PostId AS PostId,
+    so.Repo AS Repo,
+    threads2.ParentId AS ParentId
+  FROM `so-tests.2019_06_21.LinksFromSOToGH` so
+  JOIN `sotorrent-org.2019_06_21.Threads` threads2
+  ON so.PostId = threads2.PostId
+) AS so_threads 
+ON gh_threads.Repo = so_threads.Repo
+  AND gh_threads.ParentId = so_threads.ParentId
+GROUP BY Repo, FileId, FileExt, ParentId, PostId, SOUrl, GHUrl;
+
+=> so-tests.2019_06_21.MutualReferencesThread
