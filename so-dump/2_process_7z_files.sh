bf2a85be6f6ac7448b87c5a48ad3dbe0cfb1defa
Sebastian Baltes <research@sbaltes.com>
2019-09-19 11:45:04 +0930

    Only remove 7z files after successful extraction

--- so-dump/2_process_7z_files.sh
+++ so-dump/2_process_7z_files.sh
@@ -1,27 +1,17 @@
 #!/bin/bash
 
 # unzip XML files
-7za e stackoverflow.com-Badges.7z
-rm stackoverflow.com-Badges.7z
-7za e stackoverflow.com-Comments.7z
-rm stackoverflow.com-Comments.7z
-7za e stackoverflow.com-PostHistory.7z
-rm stackoverflow.com-PostHistory.7z
-7za e stackoverflow.com-PostLinks.7z
-rm stackoverflow.com-PostLinks.7z
-7za e stackoverflow.com-Posts.7z
-rm stackoverflow.com-Posts.7z
-7za e stackoverflow.com-Tags.7z
-rm stackoverflow.com-Tags.7z
-7za e stackoverflow.com-Users.7z
-rm stackoverflow.com-Users.7z
-7za e stackoverflow.com-Votes.7z
-rm stackoverflow.com-Votes.7z
+7za e stackoverflow.com-Badges.7z && rm stackoverflow.com-Badges.7z
+7za e stackoverflow.com-Comments.7z && rm stackoverflow.com-Comments.7z
+7za e stackoverflow.com-PostHistory.7z && rm stackoverflow.com-PostHistory.7z
+7za e stackoverflow.com-PostLinks.7z && rm stackoverflow.com-PostLinks.7z
+7za e stackoverflow.com-Posts.7z && rm stackoverflow.com-Posts.7z
+7za e stackoverflow.com-Tags.7z && rm stackoverflow.com-Tags.7z
+7za e stackoverflow.com-Users.7z && rm stackoverflow.com-Users.7z
+7za e stackoverflow.com-Votes.7z && rm stackoverflow.com-Votes.7z
 
 # adapt XML for MySQL import
-sed -e 's/TagBased="False"/TagBased="0"/' Badges.xml > tmp.xml
-sed -e 's/TagBased="True"/TagBased="1"/' tmp.xml > Badges.xml
-rm tmp.xml
+sed -e 's/TagBased="False"/TagBased="0"/' Badges.xml > tmp.xml && sed -e 's/TagBased="True"/TagBased="1"/' tmp.xml > Badges.xml && rm tmp.xml
 
 # compress XML files
 7za a Badges.xml.7z Badges.xml
