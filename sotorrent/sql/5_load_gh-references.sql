877809610a89912c3486061ba200186298866cf2
Sebastian Baltes <research@sbaltes.com>
2020-01-25 18:30:41 +0100

    Add new table GHCommits

--- sotorrent/sql/5_load_gh-references.sql
+++ sotorrent/sql/5_load_gh-references.sql
@@ -23,3 +23,16 @@ IGNORE 1 LINES
 (FileId, PostIds, @MatchedLine)
 SET MatchedLine = REPLACE(@MatchedLine, '&#xD;&#xA;', '\n');
 SET foreign_key_checks = 1;
+
+SET foreign_key_checks = 0;
+# load file exported from BigQuery (see also https://cloud.google.com/sql/docs/mysql/import-export/)
+LOAD DATA INFILE  '<PATH>GHCommits.csv' INTO TABLE `GHCommits`
+CHARACTER SET utf8mb4
+FIELDS TERMINATED BY ','
+OPTIONALLY ENCLOSED BY '\"'
+ESCAPED BY '\"'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES
+(CommitId, Repo, RepoOwner, RepoName, PostId, @CommentId, SOUrl, GHUrl)
+SET CommentId = nullif(@CommentId, '');
+SET foreign_key_checks = 1;
