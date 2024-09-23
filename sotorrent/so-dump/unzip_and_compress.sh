6c5e428f37f6cd1a3cf7a7621cc0467e9b698ac6
Sebastian Baltes <research@sbaltes.com>
2024-09-23 12:12:19 +0200

    Add script for new database dump structure

---
+++ sotorrent/so-dump/unzip_and_compress.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -Eeuo pipefail
+
+# unzip XML files
+7za e stackoverflow.7z
+
+# compress individual XML files
+7za a Badges.xml.7z Badges.xml
+7za a Comments.xml.7z Comments.xml
+7za a PostHistory.xml.7z PostHistory.xml
+7za a PostLinks.xml.7z PostLinks.xml
+7za a Posts.xml.7z Posts.xml
+7za a Tags.xml.7z Tags.xml
+7za a Users.xml.7z Users.xml
+7za a Votes.xml.7z Votes.xml
